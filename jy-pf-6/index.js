742f1376bd040bb801e4e2cb79c22b4e61a93e64
qwer2711 <pjy940222@gmail.com>
2020-03-22 17:46:02 +0900

    32201

--- jy-pf-6/index.js
+++ jy-pf-6/index.js
@@ -101,6 +101,57 @@ $('.part-4-mb>.head>ul>li').click(function(){
     
 });
 
+// 이모티콘 리스트 올라오기
+
+function ActiveOnVisible__init() {
+    $(window).resize(ActiveOnVisible__initOffset);
+    ActiveOnVisible__initOffset();
+
+    $(window).scroll(ActiveOnVisible__checkAndActive);
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+    $('.active-on-visible').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.offset().top;
+        $node.attr('data-active-on-visible-offsetTop', offsetTop);
+        
+        if ( !$node.attr('data-active-on-visible-diff-y') ) {
+            $node.attr('data-active-on-visible-diff-y', '0');
+        }
+        
+        if ( !$node.attr('data-active-on-visible-delay') ) {
+            $node.attr('data-active-on-visible-delay', '0');
+        }
+    });
+    
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() { 
+    $('.active-on-visible').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+        var diffY = parseInt($node.attr('data-active-on-visible-diff-y'));
+        var nodeHeight = $node.height();
+        
+        var windowHeight = $(window).height();
+        var scrollTop = $(window).scrollTop();
+        
+        if ( scrollTop + windowHeight + diffY > offsetTop && scrollTop + windowHeight < offsetTop + windowHeight + nodeHeight ) {
+            $node.addClass('active');
+        }
+        else {
+            $node.removeClass('active');
+        }
+    });
+}
+
+ActiveOnVisible__init();
+
 
 
 
